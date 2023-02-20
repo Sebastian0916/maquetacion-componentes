34ae366aa1f563ac91f44768cdfff6f645f99180
Sebastian0916 <Sebas.vera0916@gmail.com>
2023-02-20 15:49:56 -0500

    cambio lenguaje

--- maquetacion/src/app/app.module.ts
+++ maquetacion/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from 'theme/material.module';
+import { MaterialModule } from 'src/theme/material.module';
 
 @NgModule({
   declarations: [AppComponent],
