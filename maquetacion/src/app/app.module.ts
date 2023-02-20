c93b29bb8c3a3df9376807f268f7ae82388755fe
Sebastian0916 <Sebas.vera0916@gmail.com>
2023-02-20 15:31:57 -0500

    modulos and directivas

--- maquetacion/src/app/app.module.ts
+++ maquetacion/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'theme/material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    MaterialModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ maquetacion/theme/material.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatTableModule } from '@angular/material/table';
+
+const components = [
+  MatTableModule,
+  MatListModule,
+  MatButtonToggleModule,
+  MatMenuModule,
+  MatAutocompleteModule,
+  CommonModule,
+  MatButtonModule,
+  MatProgressBarModule,
+  MatToolbarModule,
+  MatChipsModule,
+  MatProgressSpinnerModule,
+  MatSliderModule,
+  MatBadgeModule,
+  MatRadioModule,
+  MatSlideToggleModule,
+  MatCheckboxModule,
+  MatStepperModule,
+  MatFormFieldModule,
+  MatSnackBarModule,
+  MatInputModule,
+  MatIconModule,
+  MatTabsModule,
+  MatDividerModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatRippleModule,
+  MatNativeDateModule,
+];
+
+@NgModule({
+  declarations: [],
+  imports: [components],
+  exports: [components],
+})
+export class MaterialModule {}
